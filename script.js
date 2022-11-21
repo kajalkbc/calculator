13930bc755e44f4372989aef172c1907328a2ab3
kajalkbc <kajalchaudhari267@gmail.com>
2022-11-21 18:40:05 +0530

    added new functionality

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
- let string="";
- 
+//  let string="";
+//  let btn = document.querySelectorAll('.button');
+//  Array.from(btn).forEach((item) => {
+//     item.addEventListener('click', (e) => {
+//         console.log(e.target);
+//     })
+//  })
+
+window.onload = function () {
+  let string = "";
+  let btn = document.querySelectorAll(".button");
+  Array.from(btn).forEach((item) => {
+    item.addEventListener("click", (e) => {
+      if (e.target.innerHTML == "=") {
+        console.log(string);
+        string = eval(string);
+
+        document.querySelector(".input").value = string;
+      } else if (e.target.innerHTML == "C") {
+        document.querySelector("input").value = "";
+        string = "";
+      } else if (e.target.innerHTML == "Del") {
+        console.log(string);
+        string = string.slice(0,-1)
+        document.querySelector(".input").value = string;
+      } else {
+        console.log(e.target.innerHTML);
+        string = string + e.target.innerHTML;
+        document.querySelector(".input").value = string;
+      }
+    });
+  });
+};
